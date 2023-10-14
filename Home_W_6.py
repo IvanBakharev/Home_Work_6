cdf3ecea46d144b2cf65190d75062b252b8a9fff
Ivan <ragna.blood82@gmail.com>
2023-10-14 17:20:02 +0300

    Complite

---
+++ Home_W_6.py
@@ -0,0 +1,20 @@
+                                                 # Задание первое
+slovo = str(input())
+
+a = slovo[::-1]
+
+if slovo == a:
+
+ print("yes")
+
+else:
+
+ print("no")
+
+                                                  #Задание второе
+
+mystring = str(input())
+
+err=' '.join(mystring.split())
+
+print(err)
